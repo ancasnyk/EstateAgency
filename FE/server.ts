8c925d54c03f9d444cb6bacac75f81158228289d
Anca Cismasiu <anca.cismasiu@snyk.io>
2026-07-22 22:46:01 +0300

    Add environment variables for services
    Added environment variables for Outlook webhook and PubNub secret key.

--- FE/server.ts
+++ FE/server.ts
@@ -17,12 +17,16 @@ export function app(): express.Express {
   server.set('view engine', 'html');
   server.set('views', browserDistFolder);
 
+  HTTPS_OUTLOOK_OFFICE_COM_WEBHOOK_MICROSOFT_OUTLOOK_TEAMS_WEBHOOK=https://outlook.office.com/webhook/db300@83eb7/IncomingWebhook/17a/11
+
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
   // Serve static files from /browser
   server.get('*.*', express.static(browserDistFolder, {
     maxAge: '1y'
   }));
+
+  SEC_C_PUBNUB_SECRET_KEY=sec-c-z98x3F13EOAkGkthkyoUStTH2v1okjTTIBVu1adldiMErPzx
 
   // All regular routes use the Angular engine
   server.get('*', (req, res, next) => {
